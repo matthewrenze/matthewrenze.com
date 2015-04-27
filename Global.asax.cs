c9dbe44f329bc8b4c81515a422d6cb63c8084521
Matthew Renze <matthew.renze@gmail.com>
2015-04-27 07:19:00 -0500

    Added Clean Architecture in ASP.NET MVC 5 article Refactored existing articles to new layout

---
+++ Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ScreamingArchitectureAspNetMvc5
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            ViewEngines.Engines.Clear();
+
+            ViewEngines.Engines.Add(new CustomRazorViewEngine());
+
+            AreaRegistration.RegisterAllAreas();
+            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
